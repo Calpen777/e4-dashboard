a6d2769f619789168fa2642d3f2ba7e5fc046fe5
Calpen777 <per.kalbhenn@gmail.com>
2026-01-01 21:33:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -60,5 +60,7 @@ def i(): return render_template("index.html")
 @app.route("/api/status")
 def s(): return jsonify(get())
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0",port=5000)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
